6a41158e7b3c3b404fcfa9b139d74645ddb18e61
nithin88844 <122030484+nithin88844@users.noreply.github.com>
2023-03-11 19:52:32 +0530

    Add files via upload

---
+++ Assignment_1.1.py
@@ -0,0 +1,15 @@
+# Q. Python is an interpreted language. Explain.
+# Q. What is the difference between lists and tuples?
+# Q. How is Memory managed in Python?
+# Q. What are Python Modules?
+# Q. What are python namespaces?
+# Q. Explain Inheritance in Python with an example?
+# Q. What is scope resolution?
+# Q. What is a dictionary in Python?
+# Q. What are functions in Python?
+# Q. What is __init__ in Python?
+# Q. What are the common built-in data types in Python?
+# Q. What are local variables and global variables in Python?
+# Q. What is type conversion in Python?
+# Q. How to install Python on Windows and set a path variable?
+# Q. What is the difference between Python Arrays and lists?
